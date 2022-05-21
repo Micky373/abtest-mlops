93135ff804ab796341134545efe74a18a3a300e5
Michael Tamirie <michaeltamirie@gmail.com>
2022-05-21 22:16:20 +0300

    Edit typo error

--- apps/model_accuracy_display.py
+++ apps/model_accuracy_display.py
@@ -3,9 +3,9 @@ import streamlit as st
 def app():
     st.title('This is the User Engagement page')
 
-    st.title('The accuracy of pur logistic regression model')
+    st.title('The accuracy of our logistic regression model')
     st.image('https://miro.medium.com/max/700/0*qsNLyfWDUcCnOA-i.png')
-    st.title('The accuracy of pur XG Boost model')
+    st.title('The accuracy of our XG Boost model')
     st.image('https://miro.medium.com/max/700/0*HRwlbEkugI5XLWis.png')
-    st.title('The accuracy of pur Random Forest model')
+    st.title('The accuracy of our Random Forest model')
     st.image('https://miro.medium.com/max/680/0*p6ScB4W_o1C6DuLw.png')
